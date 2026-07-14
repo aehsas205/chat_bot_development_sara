6d3ef6690803dfed9210d3709568a66f0c781777
Sara Maryam <227032307+saramaryam2005@users.noreply.github.com>
2026-07-14 22:19:48 +0530

    update

--- graph.py
+++ graph.py
@@ -11,4 +11,8 @@ tool_node = ToolNode(tools=tools)
 graph_builder.add_node("chatbot", chatmodel)
 graph_builder.add_node("tools", tool_node)
 
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+graph_builder.add_edge("tools", "chatbot")
 
+graph = graph_builder.compile(checkpointer=memory)
